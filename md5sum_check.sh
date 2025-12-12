b6bd7e1dd7cdcdd4b6f80de1b273ebe3b506b3b9
kenzacharlotte <108465390+kenzacharlotte@users.noreply.github.com>
2025-12-12 14:07:06 +0100

    Fix echo statements for better formatting

--- md5sum_check.sh
+++ md5sum_check.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-echo "Creating md5sum md5sum_copied\\" >> md5sum.out
+echo "Creating md5sum md5sum_copied \ " >> md5sum.out
 # Create md5sum files 
 md5sum fastqc_results/raw/*fq.gz > fastqc_results/raw/md5sum_copied.txt
-echo "md5sum_copied successfully created\\" >> md5sum.out
+echo "md5sum_copied successfully created \ " >> md5sum.out
 
 # Print the md5sum that are identical 
 #comm -12 <(awk '{print $1}' fastqc_results/raw/md5sum_copied.txt | sort) <(awk '{print $1}' fastqc_results/raw//md5sum_check.txt | sort) 
 
 # Count the number of lines (i.e. files) that are identical : this should match with the number of fastq you have in your repository
-echo "Number of similar files\\" >> md5sum.out
+echo "Number of similar files \ " >> md5sum.out
 comm -12 <(awk '{print $1}' fastqc_results/raw/md5sum_copied.txt | sort) <(awk '{print $1}' fastqc_results/raw//md5sum_check.txt | wc -l 
 
 # The arg -23 print the non matching md5sum 
 # Non-matching md5sum are sent in the loop and "grepped" in the file to print the problematic ones
 # Creates error_md5sum.out a file where all the non-matching mdf5sum and the associated files are written
 # NB : grep strands for global regular expression print
-echo "If error, non similar files\\" >> md5sum.out
+echo "If error, non similar files \ " >> md5sum.out
 comm -23 <(awk '{print $1}' fastqc_results/raw/md5sum_copied.txt  | sort) \
          <(awk '{print $1}' fastqc_results/raw//md5sum_check.txt | sort) \
 | while read md5; do
