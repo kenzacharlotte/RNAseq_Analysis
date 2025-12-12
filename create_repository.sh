aea631c84bd7b2728fe98cce01b7fa670df7952a
kenzacharlotte <108465390+kenzacharlotte@users.noreply.github.com>
2025-12-12 11:28:57 +0100

    Create directory structure for RNAseq analysis
    This script creates the directory structure for RNAseq analysis, including folders for raw data, trimmed data, FastQC results, MultiQC results, logs, and scripts.

---
+++ create_repository.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Creates repository needed for RNAseq analysis
+mkdir -p project_name/raw_data
+mkdir -p project_name/trimmed_data
+mkdir -p project_name/fastqc_results/raw
+mkdir -p project_name/fastqc_results/trimmed
+mkdir -p project_name/multiqc_results
+mkdir -p project_name/logs
+mkdir -p project_name/scripts
